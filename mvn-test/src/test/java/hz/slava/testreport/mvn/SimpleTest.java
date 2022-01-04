d40bba17643a500bcdb830a6cb099de5cf4a0a8d
Slava Sytnyk <67485185+slavagithub@users.noreply.github.com>
2022-01-04 14:36:29 +0200

    initial commit of mvn report demo

---
+++ mvn-test/src/test/java/hz/slava/testreport/mvn/SimpleTest.java
@@ -0,0 +1,18 @@
+package hz.slava.testreport.mvn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void firstTest(){
+        Assert.assertTrue("true should be true :)", true);
+    }
+
+    @Test
+    public void secondTest(){
+        Assert.assertFalse("false should be false :)", false);
+    }
+
+}
